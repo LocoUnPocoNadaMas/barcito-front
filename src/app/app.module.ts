a9a3166392ef3cf887b98ab9ddf42c29843d4351
Pato <dkno_c1@hotmail.com>
2022-08-13 20:11:03 -0300

    Subiendo a GitPages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    CarouselComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
